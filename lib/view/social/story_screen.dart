3790e85dc6ebd441633e0690115bacf57f9dec73
ankit kumar <ankitduvedi.03@gmail.com>
2023-10-07 21:01:54 +0530

    Find

--- lib/view/social/story_screen.dart
+++ lib/view/social/story_screen.dart
@@ -170,7 +170,7 @@ class StoryScreenState extends State<StoryScreen>
         const Padding(
           padding: EdgeInsets.all(16.0),
           child: Text(
-            'Communities to join:',
+            'Communities to Join:',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
         ),
@@ -187,33 +187,64 @@ class StoryScreenState extends State<StoryScreen>
 
               final pagesDocs = snapshot.data!.docs;
 
-              return ListView.builder(
-                itemCount: pagesDocs.length,
-                itemBuilder: (context, index) {
-                  final page = pagesDocs[index].data() as Map<String, dynamic>;
+              // Fetch the list of communities that the current user is following
+              return StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(userId)
+                    .collection('my_communities')
+                    .snapshots(),
+                builder: (context, followingSnapshot) {
+                  if (followingSnapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (followingSnapshot.hasError) {
+                    return Center(
+                        child: Text('Error: ${followingSnapshot.error}'));
+                  }
 
-                  return ListTile(
-                    leading: ClipRRect(
-                      borderRadius: BorderRadius.circular(mq.height * .03),
-                      child: CachedNetworkImage(
-                        width: mq.height * .055,
-                        height: mq.height * .055,
-                        fit: BoxFit.fill,
-                        imageUrl: page['image'],
-                        errorWidget: (context, url, error) =>
-                            const CircleAvatar(
-                                child: Icon(CupertinoIcons.person)),
-                      ),
-                    ),
-                    title: Text(page['name']),
-                    subtitle: Text(page['domain']),
-                    trailing: ElevatedButton(
-                      onPressed: () {
-                        // Implement the follow functionality for pages here
-                        // You can add the page to your list of followed pages in Firestore
-                      },
-                      child: const Text('Follow'),
-                    ),
+                  final followingDocs = followingSnapshot.data!.docs
+                      .map((doc) => doc.id)
+                      .toList();
+
+                  // Filter out the communities that are already followed by the user
+                  final filteredCommunities = pagesDocs.where((pageDoc) {
+                    final page = pageDoc.data() as Map<String, dynamic>;
+                    final pageId = page['id'];
+                    return !followingDocs.contains(pageId);
+                  }).toList();
+
+                  return ListView.builder(
+                    itemCount: filteredCommunities.length,
+                    itemBuilder: (context, index) {
+                      final page = filteredCommunities[index].data()
+                          as Map<String, dynamic>;
+
+                      return ListTile(
+                        leading: ClipRRect(
+                          borderRadius: BorderRadius.circular(mq.height * .03),
+                          child: CachedNetworkImage(
+                            width: mq.height * .055,
+                            height: mq.height * .055,
+                            fit: BoxFit.fill,
+                            imageUrl: page['image'],
+                            errorWidget: (context, url, error) =>
+                                const CircleAvatar(
+                                    child: Icon(CupertinoIcons.person)),
+                          ),
+                        ),
+                        title: Text(page['name']),
+                        subtitle: Text(page['domain']),
+                        trailing: ElevatedButton(
+                          onPressed: () {
+                            // Implement the follow functionality for communities here
+                            // You can add the community to your list of followed communities in Firestore
+                          },
+                          child: const Text('Follow'),
+                        ),
+                      );
+                    },
                   );
                 },
               );
