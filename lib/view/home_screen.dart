d3eea5b12b6b053cb533151f30fea5992600f5e9
ankit kumar <ankitduvedi.03@gmail.com>
2023-10-11 02:55:37 +0530

    style

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,14 +1,12 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:while_app/repository/firebase_repository.dart';
+
 import 'package:while_app/theme/pallete.dart';
 import 'package:while_app/view/create_screen.dart';
 import 'package:while_app/view/feed_screen.dart';
 import 'package:while_app/view/user_profile_screen.dart';
 import 'package:while_app/view/reels_screen.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:while_app/view/social/social_home_screen.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -20,9 +18,6 @@ class HomeScreen extends ConsumerStatefulWidget {
 class _HomeScreenState extends ConsumerState<HomeScreen> {
   // ignore: non_constant_identifier_names
   int CurrentIndex = 0;
-  final List<String> _popupMenuList = [
-    "Sign Out",
-  ];
 
   void onTapChange(int index) {
     setState(() {
@@ -46,47 +41,11 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
     return Scaffold(
       body: Stack(children: [
         Scaffold(
-          appBar: AppBar(
-            backgroundColor: currentTheme.scaffoldBackgroundColor,
-            elevation: 0.0,
-            actions: [
-              Switch.adaptive(
-                value: ref.watch(themeNotifierProvider.notifier).mode ==
-                    ThemeMode.dark,
-                onChanged: (value) => themeToggler(ref),
-              ),
-              IconButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const SocialScreen(),
-                    ));
-                  },
-                  icon: const Icon(
-                    Icons.message,
-                    color: Colors.black,
-                  )),
-              PopupMenuButton(
-                  icon: const Icon(
-                    Icons.more_vert,
-                    // color: Colors.white,
-                  ),
-                  itemBuilder: (_) => _popupMenuList.map((menuItem) {
-                        return PopupMenuItem(
-                          child: Text(menuItem),
-                          onTap: () async {
-                            FirebaseAuthMethods(FirebaseAuth.instance)
-                                .signOut(context);
-                          },
-                        );
-                      }).toList())
-            ],
-          ),
           extendBody: true,
-          backgroundColor: Color.fromARGB(255, 234, 33, 33),
+          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
           bottomNavigationBar: Container(
             decoration: BoxDecoration(
-                color: Color.fromARGB(255, 18, 18, 18),
-                borderRadius: BorderRadius.circular(0)),
+                color: Colors.black, borderRadius: BorderRadius.circular(0)),
             margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
             child: Padding(
               padding: const EdgeInsets.all(10),
